85d26bd8a4d799b05ef73ca7e60e53cf1cbd6b2b
Fandy Barestu <fandy@softwareseni.com>
2019-07-04 00:40:11 +0700

    WIP add tech redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import logReducer from './log/logReducer'
+import logReducer from './log/logReducer';
+import techReducer from './tech/techReducer';
 
 export default combineReducers({
   log: logReducer,
+  tech: techReducer,
 });
--- src/redux/tech/techTypes.js
+++ src/redux/tech/techTypes.js
@@ -1,4 +1,5 @@
 export const GET_TECHS = 'TECH/GET_TECHS';
 export const ADD_TECH = 'TECH/ADD_TECH';
 export const DELETE_TECH = 'TECH/DELETE_TECH';
+export const SET_LOADING = 'TECH/SET_LOADING';
 export const TECHS_ERROR = 'TECH/TECHS_ERROR';
